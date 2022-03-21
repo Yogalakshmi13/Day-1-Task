dc731e1e854a8f1f0bad33c7ac5f928022275e28
Yogalakshmi13 <101178481+Yogalakshmi13@users.noreply.github.com>
2022-03-21 19:40:06 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Console1
+{
+    internal class Employee
+    {
+        public static void Main()
+        {
+            Employee employee = new Employee();
+            {
+                FileStream fs = new FileStream("C:\\yoga\\programming\\Employee.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                BinaryWriter bw = new BinaryWriter(fs);
+                BinaryWriter bw1 = new BinaryWriter(fs);
+                BinaryWriter bw2 = new BinaryWriter(fs);
+                BinaryWriter bw3 = new BinaryWriter(fs);
+                BinaryWriter bw4 = new BinaryWriter(fs);
+                BinaryWriter bw5 = new BinaryWriter(fs);
+                BinaryWriter bw6 = new BinaryWriter(fs);
+                BinaryWriter bw7 = new BinaryWriter(fs);
+                bw.Write("E01");
+                bw1.Write("Nikshi");
+                bw2.Write("23");
+                bw3.Write("Female");
+                bw4.Write("E02");
+                bw5.Write("Yoga");
+                bw6.Write("23");
+                bw7.Write("Female");
+                bw.Flush();
+                fs.Close();
+                FileStream fs1 = new FileStream(@"C:\yoga\programming\Employee.txt", FileMode.Open, FileAccess.ReadWrite);
+                BinaryReader br = new BinaryReader(fs1);
+                BinaryReader br1 = new BinaryReader(fs1);
+                BinaryReader br2 = new BinaryReader(fs1);
+                BinaryReader br3 = new BinaryReader(fs1);
+                BinaryReader br4 = new BinaryReader(fs1);
+                BinaryReader br5 = new BinaryReader(fs1);
+                BinaryReader br6 = new BinaryReader(fs1);
+                BinaryReader br7 = new BinaryReader(fs1);
+                string str = br.ReadString();
+                string str1 = br1.ReadString();
+                string str2 = br2.ReadString();
+                string str3 = br3.ReadString();
+                string str4 = br4.ReadString();
+                string str5 = br5.ReadString();
+                string str6 = br6.ReadString();
+                string str7 = br7.ReadString();
+                Console.WriteLine(str);
+                Console.WriteLine(str1);
+                Console.WriteLine(str2);
+                Console.WriteLine(str3);
+                Console.WriteLine(str4);
+                Console.WriteLine(str5);
+                Console.WriteLine(str6);
+                Console.WriteLine(str7);
+                fs1.Close();
+                Console.Read();
+            }
+        }
+    }
+}
---
+++ store.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.IO;
+
+namespace Console1
+{
+    internal class store
+    {
+        public static void Main()
+        {
+            FileStream fs = new FileStream("C:\\yoga\\programming\\store.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamWriter bw = new StreamWriter(fs);
+            List<string> str = new List<String>();
+            str.Add("Gulab Jammun");
+            str.Add("Kheer");
+            str.Add("Paayasam");
+            str.Add("Bread Halwa");
+            str.Add("Mysore pak");
+            str.Add("Ney Appam");
+            foreach (string d in str)
+            {
+                bw.Write("\n" + d);
+            }
+            bw.Flush();
+            bw.Close();
+            fs.Close();
+            FileStream fs1 = new FileStream("C:\\yoga\\programming\\store.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamReader sr = new StreamReader(fs1);
+            String s = sr.ReadToEnd();
+            sr.Close();
+            fs1.Close();
+            Console.WriteLine(s);
+            FileInfo fi = new FileInfo(@"C:\yoga\programming\store.txt");
+            Console.WriteLine(fi.Length);
+            Console.WriteLine(fi.CreationTime);
+            Console.Read();
+        }
+    }
+}
