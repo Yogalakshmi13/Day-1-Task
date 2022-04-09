d0dc43d04f4be3e4bdd0eec5e468d0d0f518188c
Yogalakshmi13 <101178481+Yogalakshmi13@users.noreply.github.com>
2022-04-09 13:41:48 +0530

    Add files via upload

---
+++ anim.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Linq
+{
+    class anim
+    {
+		static int numbergoat(int Eyes, int Legs)
+		{
+			int Tot = 0;
+			Tot = (Legs) - (Eyes - 1);
+			int Total = Tot / 2;
+			return Total;
+		}
+
+		public static void Main()
+		{
+			int Eyes = 52, Legs = 80;
+			int Goat = numbergoat(Eyes, Legs);
+			Console.WriteLine("Number of Goats is:" + Goat);
+			Console.WriteLine("Number of Ducks is:" + (Goat - 2));
+		}
+	}
+	}
